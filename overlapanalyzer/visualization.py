0c655472d56a75e01cab2003836d2585cadcf9ce
Junan Lin <junan_lin@hotmail.com>
2024-08-27 17:06:01 -0400

    Added visualize_chebyshev for plotting chebyshev approximation to step function

--- overlapanalyzer/visualization.py
+++ overlapanalyzer/visualization.py
@@ -4,8 +4,8 @@ from openfermion.linalg import get_sparse_operator
 from openfermion import load_operator
 from scipy.sparse import diags
 from scipy.sparse import csr_matrix, csc_matrix
-from .read_ham import find_files, quick_load
-
+from overlapanalyzer.read_ham import find_files, quick_load
+from overlapanalyzer.polynomial_estimates import chebval, E_to_x, an_list
 # Take a general sparse matrix as input, and return a new sparse matrix with diagonal elements set to 0
 def set_diag_to_zero(sparse_matrix):
     """
@@ -48,6 +48,32 @@ def visualize_complex_matrix(matrix, save_path):
     plt.savefig(save_path, format='pdf')
     # plt.show()
     plt.close()
+
+
+def visualize_chebyshev(Emin, Emax, E0, E1, Ec=None, chebyshev_degree=10):
+    x_values = np.linspace(-1, 1, 400)
+    plt.figure(figsize=(10, 8))
+    if Ec is None:
+        Ec = (E0+E1)/2
+    x0 = E_to_x(E0, Emin, Emax)
+    x1 = E_to_x(E1, Emin, Emax)
+    xc = E_to_x(Ec, Emin, Emax)
+    # overlap_est_Chebyshev(-1.8977806459898725*2,1.1465506236600063*2,-1.8813605029470586, hlist)
+    cheb_coeffs = an_list(xc, chebyshev_degree)
+    for n in range(1, chebyshev_degree):  # Starting from 1 because cheb_coeffs[:0] would be an empty slice
+        y_values = chebval(x_values, cheb_coeffs[:n])
+        plt.plot(x_values, y_values, label=f'n = {n-1}')
+    plt.axvline(x=x0, color='r', linestyle=':', label='x0')
+    plt.axvline(x=xc, color='g', linestyle=':', label='xc')
+    plt.axvline(x=x1, color='b', linestyle=':', label='x1')
+
+    plt.title('Chebyshev Approximation of Step Function')
+    plt.xlabel('x')
+    # plt.ylabel('Chebval(x, cheb_coeffs[:n])')
+    plt.legend()
+    plt.grid(True)
+    # plt.savefig("Chebyshev_polynomials.pdf")
+    plt.show()
 
 if __name__ == "__main__":
 
