b9757694c1996f09c45ffbde3df1a5358edb34a0
Junan Lin <junan_lin@hotmail.com>
2025-01-29 16:53:18 -0500

    Added plot functions: plot_PolyDeg_vs_LB_multi, plot_Bounds_vs_PolyDeg_Multi_new, plot_Bounds_vs_PolyDeg, plot_Bounds_vs_PolyDeg_Multi, plot_inaccuracy_vs_LB. Note: some functions may be incompatible with updated PolyBounds definition, may need a rewrite in the future.

--- overlapanalyzer/visualization.py
+++ overlapanalyzer/visualization.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.ticker import MaxNLocator
+
 import numpy as np
 from openfermion.linalg import get_sparse_operator
 from openfermion import load_operator
@@ -6,7 +9,14 @@ from scipy.sparse import diags
 from scipy.sparse import csr_matrix, csc_matrix
 from overlapanalyzer.read_ham import find_files, quick_load
 from overlapanalyzer.polynomial_estimates import chebval, E_to_x, an_list
-# Take a general sparse matrix as input, and return a new sparse matrix with diagonal elements set to 0
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.sans-serif": "Computer Modern Roman",
+    "font.size": 16,
+})
+
 def set_diag_to_zero(sparse_matrix):
     """
     Takes a sparse matrix and returns a new sparse matrix with diagonal elements set to 0.
@@ -74,6 +84,235 @@ def visualize_chebyshev(Lower, Upper, E0, E1, Ec=None, chebyshev_degree=10):
     # plt.savefig("Chebyshev_polynomials.pdf")
     plt.show()
 
+
+def plot_PolyDeg_vs_LB_multi(data):
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[4, 1])
+    fig.subplots_adjust(hspace=0.05)  # adjust space between Axes
+    # ax = plt.gca()
+    legend_elements = [Line2D([0], [0], color='grey', lw=1.5),
+                       Line2D([0], [0], color='grey', linestyle='-.', lw=1.5),
+                       Line2D([0], [0], color='grey', linestyle=':', lw=1.5)]
+
+    for i, key in enumerate(data.keys()):
+        if i == 0:
+            current_lowest_Eckart = data[key]['Eckart']
+        color = ax1._get_lines._cycler_items[i]['color']
+        x_data = np.array([i for i in range(1, len(data[key]['all_Overlap_LBs'])+1)])
+        ax1.plot(x_data, data[key]['all_Overlap_LBs'], marker='*', color=color, label='Lower Bound')
+        ax1.axhline(y=data[key]['exact_Overlap'], linestyle='-.', color=color, label=r'$P_{exact}$')
+        ax1.axhline(y=data[key]['Eckart'], linestyle=':', color=color, label='Eckart')
+        ax2.plot(x_data, data[key]['all_Overlap_LBs'], marker='*', color=color, label=f'{key} LBs')
+        ax2.axhline(y=data[key]['exact_Overlap'], linestyle='-.', color=color, label=f'{key}'+r' $P_{exact}$')
+        ax2.axhline(y=data[key]['Eckart'], linestyle=':', color=color, label=f'{key} Eckart')
+        if data[key]['Eckart'] < current_lowest_Eckart:
+            current_lowest_Eckart = data[key]['Eckart']
+    
+    ax1.set_ylim(-0.05, 1)
+    ax2.set_ylim(current_lowest_Eckart-0.02, current_lowest_Eckart+0.02)
+    # hide the spines between ax and ax2
+    ax1.spines.bottom.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    ax1.xaxis.tick_top()
+    ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+    ax2.xaxis.tick_bottom()
+
+    d = .3  # proportion of vertical to horizontal extent of the slanted line
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+    ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+    ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+    plt.xlabel('Polynomial degree')
+    ax1.set_ylabel('Overlap value')
+    ax1.legend(legend_elements, ['Lower Bound', r'$P_{exact}$', 'Eckart Bound'])
+    plt.show()
+
+def plot_Bounds_vs_PolyDeg_Multi_new(data_dict_list, exactCons_data_dict_list, MaxDegree_list):
+    # Font size variables
+    label_fontsize = 18
+    molecule_fontsize = 18
+    label_padding = 3  # Padding for y-axis labels
+
+    fig, axs = plt.subplots(len(data_dict_list), 2, figsize=(12, 8), sharex=True, sharey=True)
+
+    # Increase spacing between subplots
+    fig.subplots_adjust(wspace=0.4)  # Adjust horizontal spacing
+
+    # Molecule names for labeling subplots
+    molecule_names = [r'H$_4$', r'H$_4$', r'H$_2$O', r'H$_2$O']
+
+    for row, data_dict in enumerate(data_dict_list):
+        exactCons_data_dict = exactCons_data_dict_list[row]
+        
+        for key in [0, 1]:
+            max_deg = MaxDegree_list[row][key]
+            x_data = np.array([i for i in range(1, max_deg)])
+            axs[row, key].xaxis.set_major_locator(MaxNLocator(integer=True))
+
+            # Plot red markers (eigenvalue bounds)
+            data1 = np.array(data_dict[str(key)]['Overlap_LBs'][:max_deg-1])
+            mask = ~np.isnan(data1) 
+            axs[row, key].plot(x_data[mask], data1[mask], marker='.', linestyle=':', label='Lower Bound (Approx.)', color='red')
+            data2 = np.array(data_dict[str(key)]['Overlap_UBs'][:max_deg-1])
+            mask = ~np.isnan(data2) 
+            axs[row, key].plot(x_data[mask], data2[mask], marker='v', linestyle=':', label='Upper Bound (Approx.)', color='red')
+
+            # Plot green markers (exact eigenvalues as constraints)
+            data3 = np.array(exactCons_data_dict[str(key)]['Overlap_LBs'][:max_deg-1])
+            mask = ~np.isnan(data3)
+            axs[row, key].plot(x_data[mask], data3[mask], marker='.', linestyle=':', label='Lower Bound (Exact)', color='green')
+            data4 = np.array(exactCons_data_dict[str(key)]['Overlap_UBs'][:max_deg-1])
+            mask = ~np.isnan(data4)
+            axs[row, key].plot(x_data[mask], data4[mask], marker='v', linestyle=':', label='Upper Bound (Exact)', color='green')
+
+            # Plot blue horizontal line (exact overlap)
+            axs[row, key].axhline(y=data_dict[str(key)]['exact_ovlp'], linestyle='-', label='Exact Overlap', color='blue')
+
+            # Set subplot limits
+            axs[row, key].set_ylim(-0.05, 1.0)
+            axs[row, key].set_yticks([0.0, 0.5, 1.0])
+
+            # Adjust x-axis limits
+            if key == 0:  # Left plots
+                axs[row, key].set_xlim(0, 21)
+            else:  # Right plots
+                axs[row, key].set_xlim(0, max_deg)
+
+            # Add molecule label to top right corner
+            axs[row, key].text(
+                0.95, 0.95, molecule_names[row * 2 + key],
+                transform=axs[row, key].transAxes,
+                fontsize=molecule_fontsize,
+                verticalalignment='top', horizontalalignment='right'
+            )
+
+    # Y-axis labels with increased padding
+    axs[0, 0].set_ylabel(r'$P_0$', fontsize=label_fontsize, labelpad=label_padding)
+    axs[1, 0].set_ylabel(r'$P_0$', fontsize=label_fontsize, labelpad=label_padding)
+    axs[0, 1].set_ylabel(r'$P_1$', fontsize=label_fontsize, labelpad=label_padding)
+    axs[1, 1].set_ylabel(r'$P_1$', fontsize=label_fontsize, labelpad=label_padding)
+
+    # X-axis labels
+    axs[1, 0].set_xlabel('Polynomial Degree', fontsize=label_fontsize)
+    axs[1, 1].set_xlabel('Polynomial Degree', fontsize=label_fontsize)
+
+    # Force y-axis tick labels to show on all plots
+    for ax in axs.flat:
+        ax.tick_params(axis='y', which='both', labelleft=True)
+
+    # Place legend in the top-right subplot
+    axs[0, 1].legend(loc='best', fontsize=10)
+
+    plt.tight_layout(rect=[0.05, 0.05, 1, 0.95])
+    plt.show()
+
+
+def plot_Bounds_vs_PolyDeg(data, molname):
+    for i, key in enumerate(data.keys()):
+        if data[key]['Eckart'] > 0:
+            x_data = np.array([i for i in range(1, len(data[key]['Overlap_LBs'])+1)])
+            plt.plot(x_data, data[key]['Overlap_LBs'], marker='.', linestyle=':', label=f'Lower Bound', color='red')
+            plt.plot(x_data, data[key]['Overlap_UBs'], marker='v', linestyle=':', label='Upper Bound', color='red')
+            plt.axhline(y=data[key]['exact_Overlap'], linestyle='-', label=r' $P_{exact}$', color='blue')
+            plt.axhline(y=data[key]['Eckart'], linestyle='-.', label='Eckart Bound', color='blue')
+            plt.xticks(x_data)
+            plt.xlabel('Polynomial degree')
+            plt.ylabel('Overlap value')
+            plt.legend()
+            # plt.title(molname + f', R = {key}')
+            plt.show()
+        else:
+            fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[4, 1])
+            fig.subplots_adjust(hspace=0.05)  # adjust space between Axes
+            # ax = plt.gca()
+
+
+            x_data = np.array([i for i in range(1, len(data[key]['Overlap_LBs'])+1)])
+            ax1.plot(x_data, data[key]['Overlap_LBs'], marker='.', linestyle=':', color='red', label='Lower Bound')
+            ax1.plot(x_data, data[key]['Overlap_UBs'], marker='v', linestyle=':', color='red', label='Upper Bound')
+            ax1.axhline(y=data[key]['exact_Overlap'], linestyle='-', color='blue', label=r'$P_{exact}$')
+            ax1.axhline(y=data[key]['Eckart'], linestyle='-.', color='blue', label='Eckart Bound')
+            # ax2.plot(x_data, data[key]['Overlap_LBs'], marker='.', linestyle=':', color=color)
+            # ax2.axhline(y=data[key]['exact_Overlap'], linestyle='-', color=color)
+            ax2.axhline(y=data[key]['Eckart'], linestyle='-.', color='blue')
+
+            
+            ax1.set_ylim(-0.05, 1.05)
+            ax2.set_ylim(data[key]['Eckart']-0.02, data[key]['Eckart']+0.02)
+            # hide the spines between ax and ax2
+            ax1.spines.bottom.set_visible(False)
+            ax2.spines.top.set_visible(False)
+            ax1.xaxis.tick_top()
+            ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+            ax1.set_xticks([])
+            ax1.set_xticks([], minor=True)
+            ax2.xaxis.tick_bottom()
+
+            d = .3  # proportion of vertical to horizontal extent of the slanted line
+            kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
+                        linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+            ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+            ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+            ax2.set_xticks(x_data)
+            ax2.set_xlabel('Polynomial degree')
+            ax1.set_ylabel('Overlap value')
+            ax1.legend()
+            # ax1.set_title(molname + f', R = {key}')
+            plt.show()
+
+def plot_Bounds_vs_PolyDeg_Multi(data_dict_list, exactCons_data_dict_list, MaxDegree_list):
+    fig, axs = plt.subplots(len(data_dict_list),2)
+
+    for row, data_dict in enumerate(data_dict_list):
+        exactCons_data_dict = exactCons_data_dict_list[row]
+        for key in [0, 1]:
+            max_deg = MaxDegree_list[row][key]
+            x_data = np.array([i for i in range(1, max_deg)])
+            axs[row, key].xaxis.set_major_locator(MaxNLocator(integer=True))
+
+            data1 = np.array(data_dict[str(key)]['Overlap_LBs'][:max_deg-1])
+            mask = ~np.isnan(data1) 
+            axs[row, key].plot(x_data[mask], data1[mask], marker='.', linestyle=':', label='Lower Bound', color='red')
+            data2 = np.array(data_dict[str(key)]['Overlap_UBs'][:max_deg-1])
+            mask = ~np.isnan(data2) 
+            axs[row, key].plot(x_data[mask], data2[mask], marker='v', linestyle=':', label='Upper Bound', color='red')
+            data3 = np.array(exactCons_data_dict[str(key)]['Overlap_LBs'][:max_deg-1])
+            mask = ~np.isnan(data3)
+            axs[row, key].plot(x_data[mask], data3[mask], marker='.', linestyle=':', label='Lower Bound', color='green')
+            data4 = np.array(exactCons_data_dict[str(key)]['Overlap_UBs'][:max_deg-1])
+            mask = ~np.isnan(data4)
+            axs[row, key].plot(x_data[mask], data4[mask], marker='v', linestyle=':', label='Upper Bound', color='green')
+            axs[row, key].axhline(y=data_dict[str(key)]['exact_ovlp'], linestyle='-', label=r' $P_{exact}$', color='blue')
+            # plt.axhline(y=data_dict[str(key)]['Eckart'], linestyle='-.', label='Eckart Bound', color='blue')
+            # axs[row, key].set_xticks(x_data)
+            # axs[row, key].xlabel('Polynomial degree')
+            # axs[row, key].ylabel('Overlap value')
+            # axs[row, key].legend()
+            # plt.title(molname + f', R = {key}')
+    plt.tight_layout()
+    plt.show()
+
+def plot_inaccuracy_vs_LB(data):
+    for i, key in enumerate(data.keys()):
+        inaccuracies = np.array(data[key]['inaccuracies'])*2/data[key]['gap']
+        O_exact = data[key]['O_exact']
+        for pol_deg in data[key]["Overlap_LBs"].keys():
+            y_data = np.abs(np.array(data[key]["Overlap_LBs"][pol_deg]) - O_exact) / O_exact
+            plt.semilogy(inaccuracies, y_data, marker='.', linestyle=':', label=pol_deg)
+        plt.legend()
+        plt.ylabel('Relative error')
+        plt.xlabel('Energy Inaccuracy')
+        plt.show()
+        plt.close()
+        O_exact = data[key]['O_exact']
+        for pol_deg in data[key]["Overlap_UBs"].keys():
+            y_data = np.abs(np.array(data[key]["Overlap_UBs"][pol_deg]) - O_exact) / O_exact
+            plt.semilogy(inaccuracies, y_data, marker='.', linestyle=':', label=pol_deg)
+        plt.legend()
+        plt.ylabel('Relative error')
+        plt.xlabel('Energy Inaccuracy')
+        plt.show()
+        plt.close()
+        
 if __name__ == "__main__":
 
     dir = 'hamiltonian_gen_test/h2o/4e4o'
